a03817efea66d85fa3e60ded0a9adfd2577a8a2a
ymamamotokazunori <gaogaigar673@gmail.com>
2021-04-06 10:57:52 +0000

    [Start]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '727812f06b1bc397c8ddbfc6a824050dd1fc9f681397c212fab70059957081eb8c80dbca1642d3d3ac2d62c143bc4f7647e8fb11e9e6dbb7955b5c4cc4aff523'
