ec92eacf052c0720aa37bad7e7cf2f0eb065ac78
ymamamotokazunori <gaogaigar673@gmail.com>
2021-04-07 07:34:36 +0000

    [add]ユーザ詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
-
     resources :post_comments, only: [:create, :destroy]
   end
 
